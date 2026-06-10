286b288541d32ff81c32c85db28dc0502983877e
Maria Trushanina <mmalafeeva@yandex.ru>
2026-06-10 23:16:58 +0300

    feat: add h2 mem for friend's testing

--- src/main/java/com/socialmedia/controllers/UserController.java
+++ src/main/java/com/socialmedia/controllers/UserController.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -43,7 +44,7 @@ public class UserController {
     }
 
     @PutMapping("/{userId}")
-    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public UserDto updateUser(@PathVariable Long userId, @Valid @RequestBody RegistrationDto dto) {
         log.info("Получен запрос Put на обновление пользователя по id {}", userId);
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-DELETE
-FROM roles;
-INSERT INTO roles(name)
-values ('ROLE_USER'),
-       ('ROLE_ADMIN');
